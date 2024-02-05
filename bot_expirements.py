b25db7043c2f27bc1ac2d48656fcb7d636457ea8
kefiroff <152662726+kefiroff@users.noreply.github.com>
2024-02-05 18:51:33 +0300

    Add files via upload

---
+++ bot_expirements.py
@@ -0,0 +1,73 @@
+import telebot
+import db
+bot = telebot.TeleBot("6565609556:AAFNrI9AkyE18D56J-xeN4LYzwvrtfUttE4", parse_mode=None)
+
+
+def add_user(id, name):
+    db.dbadd_user(id, name)
+
+
+def add_title(title, id, name):
+    db.dbadd_exercise_title(title, id, name)
+
+
+def add_exercise_name(title, id, name, exercise_name):
+    db.dbadd_exercise_name(title, id, name, exercise_name)
+
+
+def get_exercise_name(title, id):
+    return db.dbget_exercise_name(title, id)
+
+
+@bot.message_handler(commands=['start'])
+def start(m):
+    add_user(m.chat.id, m.from_user.first_name)
+    bot.reply_to(m, 'Привет! Я помогу тебе с тренировками!')
+    bot.send_message(m.chat.id, 'Нажми:\n/help, если что-то непонятно\n/add чтобы добавить информацию о тренировке\n/get получить информацию о тренировке\n/change изменить данные тренировки')
+
+
+@bot.message_handler(commands=['help'])
+def reply_help(m):
+    bot.reply_to(m, 'Нажми /add чтобы добавить информацию о тренировке\nНажмите /get чтобы получить информацию о тренировке\nНажмите /change чтобы изменить данные тренировки')
+
+
+@bot.message_handler(commands=['add'])
+def reply_add(m):
+    msg_title = bot.reply_to(m, 'Введи название тренировки')
+    bot.register_next_step_handler(msg_title, step_set_title)
+
+
+def step_set_title(m):
+    global user_title
+    user_title = m.text
+    bot.send_message(m.chat.id, 'Название добавлено')
+    msg_exercise_name = bot.send_message(m.chat.id, 'Введи название упражнений через запятую и пробел')
+    bot.register_next_step_handler(msg_exercise_name, step_set_exercise_name)
+
+
+def step_set_exercise_name(m):
+    exercise_name = m.text
+    exercise_name = exercise_name.split(', ')
+    add_exercise_name(user_title, m.chat.id, m.from_user.first_name, exercise_name)
+    bot.send_message(m.chat.id, 'Названия добавлены')
+
+
+@bot.message_handler(commands=['get'])
+def reply_add(m):
+    msg_title = bot.reply_to(m, 'Введи название тренировки')
+    bot.register_next_step_handler(msg_title, step_get_title)
+
+
+def step_get_title(m):
+    user_title = m.text
+    exercise_name = get_exercise_name(user_title, m.chat.id)
+    for name in exercise_name:
+        bot.send_message(m.chat.id, name)
+
+
+@bot.message_handler(commands=['stop'])
+def reply_stop(m):
+    bot.stop_bot()
+
+
+bot.polling()
---
+++ db.py
@@ -0,0 +1,101 @@
+import mariadb
+
+
+def dbadd_user(uid, uname):
+    try:
+        conn = mariadb.connect(
+            user='root',
+            password='kachalka',
+            host='127.0.0.1',
+            port=3306,
+            database='kachalka'
+        )
+        cur = conn.cursor()
+        values = (uid, uname)
+        mariadb_insert_query = 'INSERT INTO users (tele_id, name) VALUES (%s, %s)'
+        cur.execute(mariadb_insert_query, values)
+        conn.commit()
+        print('Пользователь добавлен')
+        cur.close()
+    except mariadb.Error as error:
+        print('Ошибка при подключении:', error)
+    finally:
+        if (conn):
+            conn.close()
+            print('Соединение закрыто')
+
+
+def dbadd_exercise_title(dbtitle, uid, uname):
+    try:
+        conn = mariadb.connect(
+            user='root',
+            password='kachalka',
+            host='127.0.0.1',
+            port=3306,
+            database='kachalka'
+        )
+        cur = conn.cursor()
+        value = (uid, uname, dbtitle)
+        mariadb_insert_query = 'INSERT INTO users (tele_id, name, exercise_title) VALUES (?, ?, ?)'
+        cur.execute(mariadb_insert_query, value)
+        conn.commit()
+        print('Название добавлено')
+        cur.close()
+    except mariadb.Error as error:
+        print('Ошибка при подключении:', error)
+    finally:
+        if (conn):
+            conn.close()
+            print('Соединение закрыто')
+
+
+def dbadd_exercise_name(dbtitle, uid, uname, dbexercise_name):
+    try:
+        conn = mariadb.connect(
+            user='root',
+            password='kachalka',
+            host='127.0.0.1',
+            port=3306,
+            database='kachalka'
+        )
+        cur = conn.cursor()
+        value = []
+        for name in dbexercise_name:
+            tuple_data = (uid, uname, dbtitle, name)
+            value.append(tuple_data)
+        mariadb_insert_query = 'INSERT INTO users (tele_id, name, exercise_title, exercise_name) VALUES (?, ?, ?, ?)'
+        cur.executemany(mariadb_insert_query, value)
+        conn.commit()
+        print('Название добавлено')
+        cur.close()
+    except mariadb.Error as error:
+        print('Ошибка при подключении:', error)
+    finally:
+        if (conn):
+            conn.close()
+            print('Соединение закрыто')
+
+
+def dbget_exercise_name(dbtitle, uid):
+    try:
+        conn = mariadb.connect(
+            user='root',
+            password='kachalka',
+            host='127.0.0.1',
+            port=3306,
+            database='kachalka'
+        )
+        cur = conn.cursor()
+        value = (dbtitle, uid)
+        text = []
+        mariadb_insert_query = 'SELECT exercise_name FROM users WHERE exercise_title = ? and tele_id = ?'
+        cur.execute(mariadb_insert_query, value)
+        exercise_name = cur.fetchall()
+        cur.close()
+        return exercise_name
+    except mariadb.Error as error:
+        print('Ошибка при подключении:', error)
+    finally:
+        if (conn):
+            conn.close()
+            print('Соединение закрыто')
